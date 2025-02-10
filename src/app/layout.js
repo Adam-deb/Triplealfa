836eef7f361032932aba80a5fe98339ab40aeb4b
adam-deb <152192245+Adam-deb@users.noreply.github.com>
2025-02-10 11:34:58 +0000

    adjusted favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,8 +2,8 @@ import "./globals.css";
 
 
 export const metadata = {
-  title: "TripleAlfa | Invest in Whisky Casks and Media Funds for High-Growth Opportunities",
-  description: "Explore exclusive whisky cask and media fund investment opportunities with TripleAlfa. We connect you to innovative alternative assets.",
+  title: "TripleAlfa | Invest in Whisky Casks",
+  description: "Explore exclusive whisky cask with TripleAlfa. We connect you to innovative alternative assets.",
 };
 
 export default function RootLayout({ children }) {
