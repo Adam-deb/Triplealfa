a96e2c8868ec4a1c8210ff58d823f3f0e17cfdac
adam-deb <152192245+Adam-deb@users.noreply.github.com>
2025-03-10 10:36:20 +0000

    updated new signature

--- src/app/fahad-signature/page.js
+++ src/app/fahad-signature/page.js
@@ -1,5 +1,6 @@
 export default function Signature1() {
   return (
+    <>
     <table
       width="100%"
       cellSpacing="0"
@@ -51,7 +52,7 @@ export default function Signature1() {
                         <strong>M:</strong> +44 7468 543618
                       </a><br />
                       <a href="mailto:adam@triplealfa.com" style={{ color: 'black', textDecoration: 'none', fontFamily: 'Arial, sans-serif' }}>
-                        <strong>E:</strong> fahad@triplealfa.com
+                        <strong>E:</strong> fahad.haris@triplealfa.com
                       </a>
                     </p>
                     <p style={{ margin: '0', fontSize: '14px', color: '#555' }}>
@@ -67,5 +68,13 @@ export default function Signature1() {
         </tr>
       </tbody>
     </table>
+    <div style={{ maxWidth: '600px', fontFamily: 'Arial, sans-serif' }}>
+    <p style={{ fontFamily: 'Arial, sans-serif', marginTop: '20px', fontSize: '12px' }}>Disclaimer: The content of this email is intended for the person or entity to which it is addressed only. This email may contain confidential information. If you are not the person to whom this message is addressed, be aware that any use, reproduction, or distribution of this message is strictly prohibited. If you received this in error, please contact the sender and immediately delete this email and any attachments. Triple Alfa shall have no liability for any misuse or unauthorised distribution of this Information.</p>
+
+    <p style={{ fontFamily: 'Arial, sans-serif', marginTop: '20px', fontSize: '12px' }}>No employee of Triple Alfa has the authority to conclude any binding contract without an explicit written separate agreement and express written confirmation.</p>
+
+    <p style={{ fontFamily: 'Arial, sans-serif', marginTop: '20px', fontSize: '12px' }}>The Information is not investment advice and does not constitute any offer or solicitation to subscribe or redeem. Investment involves risk. Past performance is not indicative of future performance. Investor should refer to the offering documentation of the product(s) for detailed information (including risk factors) prior to investing in the product(s). If you have any query on the above information or any product offering documentation, you should seek independent professional advice.</p>
+    </div>
+    </>
   );
 }
