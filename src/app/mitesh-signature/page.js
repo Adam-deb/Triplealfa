21f8e5d32d5ed340ef96e95f86b2692f9e6e562d
adam-deb <152192245+Adam-deb@users.noreply.github.com>
2025-08-15 12:10:11 +0100

    added mitesh's signature

---
+++ src/app/mitesh-signature/page.js
@@ -0,0 +1,176 @@
+export default function Signature1() {
+  return (
+    <>
+    <table
+      width="100%"
+      cellSpacing="0"
+      cellPadding="0"
+      border="0"
+      style={{ maxWidth: '600px', fontFamily: 'Arial, sans-serif' }}
+    >
+      <tbody>
+        <tr>
+          <td>
+            <table width="100%" cellSpacing="0" cellPadding="0" border="0">
+              <tbody>
+                <tr>
+                  <td width="100" style={{ paddingRight: '10px', verticalAlign: 'middle' }}>
+                    <img
+                      src="/email-signatures/headshots/Mitesh.png"
+                      alt="headshot"
+                      width="100"
+                      height="100"
+                      style={{ borderRadius: '50%', display: 'block', maxWidth: '100px', height: 'auto' }}
+                    />
+                  </td>
+                  <td style={{ verticalAlign: 'middle', width: '100%', fontFamily: 'Arial, sans-serif' }}>
+                    <table width="100%" border="0" cellSpacing="0" cellPadding="0" style={{ borderBottom: '1px solid #333', paddingBottom: '10px' }}>
+                      <tbody>
+                        <tr>
+                          <td style={{ textAlign: 'left', verticalAlign: 'middle' }}>
+                            <p style={{ margin: '0', fontSize: '20px', fontWeight: 'bold', color: '#333', fontFamily: 'Arial, sans-serif' }}>
+                              Mitesh Dave
+                            </p>
+                            <p style={{ margin: '2px 0 0 0', fontSize: '14px', color: '#333', fontFamily: 'Arial, sans-serif' }}>
+                              Non Executive Director | Triple Alfa
+                            </p>
+                          </td>
+                          <td style={{ textAlign: 'right', verticalAlign: 'middle' }}>
+                            <img
+                              src="/black_logo.png"
+                              alt="Triple Alfa logo"
+                              width="75"
+                              height="45"
+                              style={{ maxWidth: '75px', height: 'auto' }}
+                            />
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                    <p style={{ margin: '5px 0 0', fontSize: '14px', color: '#555' }}>
+                      <a href="tel:+919820349399" style={{ color: 'black', textDecoration: 'none', fontFamily: 'Arial, sans-serif' }}>
+                        <strong>M:</strong> +91 98203 49399
+                      </a><br />
+                      <a href="mailto:mitesh@tripelalfa.com" style={{ color: 'black', textDecoration: 'none', fontFamily: 'Arial, sans-serif' }}>
+                        <strong>E:</strong> mitesh@tripelalfa.com
+                      </a>
+                    </p>
+                    <p style={{ margin: '0', fontSize: '14px', color: '#555' }}>
+                      <a href="https://triplealfa.com" style={{ color: 'black', textDecoration: 'none', fontFamily: 'Arial, sans-serif' }}>
+                        <strong>W:</strong> triplealfa.com
+                      </a>
+                    </p>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+              {/* Separate section for company contact details and social media icons */}
+              <div
+        style={{
+          maxWidth: '600px',
+          fontFamily: 'Arial, sans-serif',
+          marginTop: '0px',
+          borderTop: '1px solid #333',
+          paddingTop: '5px',
+        }}
+      >
+        <table width="100%" cellSpacing="0" cellPadding="0" border="0">
+          <tbody>
+            <tr>
+              {/* Company phone and email on the left */}
+              <td style={{ verticalAlign: 'middle', paddingRight: '10px', textAlign: 'left' }}>
+                <p
+                  style={{
+                    fontSize: '14px',
+                    color: '#555',
+                    display: 'flex',
+                    alignItems: 'center',
+                    fontFamily: 'Arial, sans-serif',
+                  }}
+                >
+                  <img
+                    src="/icons/phone.png"
+                    alt="Phone"
+                    width="16"
+                    height="16"
+                    style={{ marginRight: '8px' }}
+                  />
+                  <a
+                    href="tel:+441183152274"
+                    style={{ color: 'black', textDecoration: 'none', fontFamily: 'Arial, sans-serif' }}
+                  >
+                    +44 118 315 2274
+                  </a>
+                </p>
+              </td>
+              <td style={{ verticalAlign: 'middle', textAlign: 'left' }}>
+                <p
+                  style={{
+                    fontSize: '14px',
+                    color: '#555',
+                    display: 'flex',
+                    alignItems: 'center',
+                    fontFamily: 'Arial, sans-serif',
+                  }}
+                >
+                  <img
+                    src="/icons/email.png"
+                    alt="Email"
+                    width="16"
+                    height="16"
+                    style={{ marginRight: '8px' }}
+                  />
+                  <a
+                    href="mailto:info@triplealfa.com"
+                    style={{ color: 'black', textDecoration: 'none', fontFamily: 'Arial, sans-serif' }}
+                  >
+                    info@triplealfa.com
+                  </a>
+                </p>
+              </td>
+
+              {/* Social media icons on the right */}
+              <td style={{ textAlign: 'right', verticalAlign: 'middle' }}>
+                <a href="https://www.linkedin.com/company/triple-alfa/" style={{ marginRight: '10px' }}>
+                  <img src="/icons/linkedIn.png" alt="LinkedIn" width="20" height="20" />
+                </a>
+                <a href="https://www.instagram.com/triplealfa_uk/" style={{ marginRight: '10px' }}>
+                  <img src="/icons/instagram.png" alt="Instagram" width="20" height="20" />
+                </a>
+                <a href="https://www.facebook.com/profile.php?id=61573655012123" style={{ marginRight: '10px' }}>
+                  <img src="/icons/facebook.png" alt="Facebook" width="20" height="20" />
+                </a>
+                <a href="https://x.com/triplealfa_uk">
+                  <img src="/icons/X.png" alt="Twitter" width="20" height="20" />
+                </a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      {/* Disclaimer section */}
+      <div
+        style={{
+          maxWidth: '600px',
+          fontFamily: 'Arial, sans-serif',
+          marginTop: '15px',
+        }}
+      >
+        <p style={{ fontSize: '12px', marginTop: '20px', fontFamily: 'Arial, sans-serif' }}>
+          Disclaimer: The content of this email is intended for the person or entity to which it is addressed only. This email may contain confidential information. If you are not the person to whom this message is addressed, be aware that any use, reproduction, or distribution of this message is strictly prohibited. If you received this in error, please contact the sender and immediately delete this email and any attachments. Triple Alfa shall have no liability for any misuse or unauthorised distribution of this information.
+        </p>
+        <p style={{ fontSize: '12px', marginTop: '20px', fontFamily: 'Arial, sans-serif' }}>
+          No employee of Triple Alfa has the authority to conclude any binding contract without an explicit written separate agreement and express written confirmation.
+        </p>
+        <p style={{ fontSize: '12px', marginTop: '20px', fontFamily: 'Arial, sans-serif' }}>
+          The information is not investment advice and does not constitute any offer or solicitation to subscribe or redeem. Investment involves risk. Past performance is not indicative of future performance. Investors should refer to the offering documentation of the product(s) for detailed information (including risk factors) prior to investing in the product(s). If you have any query on the above information or any product offering documentation, you should seek independent professional advice.
+        </p>
+      </div>
+    </>
+  );
+}
